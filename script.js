03ba688ade0a445f5ead824b47e8ee835e7bec1a
cncrown10-droid <cncrown10@gmail.com>
2025-11-29 22:18:45 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+<script>
+let images = [
+  "images/photo1.jpg",
+  "images/photo2.jpg",
+  "images/photo3.jpg"
+  // Ajoute ici tes autres images si nécessaire
+];
+
+let currentIndex = 0;
+
+function openLightbox(index) {
+  currentIndex = index;
+  document.getElementById('lightbox-img').src = images[currentIndex];
+  document.getElementById('lightbox').style.display = "flex";
+}
+
+function closeLightbox() {
+  document.getElementById('lightbox').style.display = "none";
+}
+
+function nextImage() {
+  currentIndex = (currentIndex + 1) % images.length;
+  document.getElementById('lightbox-img').src = images[currentIndex];
+}
+
+function prevImage() {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  document.getElementById('lightbox-img').src = images[currentIndex];
+}
+
+/* Fermer en cliquant en dehors de l'image */
+document.getElementById('lightbox').addEventListener("click", function(e){
+  if (e.target.id === "lightbox") {
+    closeLightbox();
+  }
+});
+</script>
